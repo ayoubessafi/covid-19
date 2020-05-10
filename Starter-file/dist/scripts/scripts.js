a17599654d5f52ecc442a3987045792cd7356c0c
ayoubessafi <57940108+ayoubessafi@users.noreply.github.com>
2020-05-10 23:11:27 +0000

    add gulp scripts and the folder test

---
+++ Starter-file/dist/scripts/scripts.js
@@ -0,0 +1,82 @@
+const start = document.querySelector(".start");
+const stepers = document.querySelectorAll(".stepers");
+const preambule = document.querySelector(".Preambule");
+const contentQuestion = document.querySelector(".test");
+const questionnes = document.querySelectorAll(".questionnes");
+const next = document.querySelector(".next");
+const precedent = document.querySelector(".precedent");
+const barProgress = document.querySelector(".w3-blue");
+const choix = document.querySelectorAll('.choix');
+const vraisChoix = document.querySelectorAll(".vrais");
+const fauxChoix = document.querySelectorAll(".faux");
+const btnResule = document.querySelector(".rslBtn");
+const questionProgres = document.querySelector(".progres");
+const resultFinal = document.querySelector(".rsltMsg");
+// console.log(questionnes);
+
+// console.log(stepers);
+// console.log(stepers[0]);
+
+start.addEventListener("click", firstStep);
+
+function firstStep() {
+    start.style.display = "none";
+    stepers[0].classList.remove("shadow");
+    stepers[1].classList.add("shadow");
+    preambule.style.display = "none";
+    contentQuestion.classList.remove("hide");
+    // questionnes.classList.add('remove1');
+    // presedentButton();
+    
+}
+
+
+let i = 1;
+next.addEventListener("click", () => {
+    if (i === 0) {
+        questionnes[i].classList.remove('remove');
+    } else if (i === questionnes.length){
+        next.style.display="none";
+        precedent.style.display="none";
+        btnResule.classList.remove('hide');
+    }else {
+        questionnes[i].classList.remove('remove');
+        questionnes[i - 1].classList.add('remove');
+        precedent.classList.remove('hide');
+
+    }
+   
+    i++;
+    progress(i-1) 
+})
+
+precedent.addEventListener("click", () => {
+        questionnes[i].classList.add('remove');
+        questionnes[i - 1].classList.remove('remove');
+    i--;
+    if(i === 0){
+        precedent.classList.add('hide');
+    }
+
+
+    progress(i-1) 
+})
+
+function progress(number) {
+    const numberProgres = number + 1;
+    questionProgres.innerText = numberProgres;
+    barProgress.style.width = `calc(${numberProgres} * calc(100% / 5))`;
+
+}
+
+
+btnResule.addEventListener("click" , ()=>{
+    stepers[1].classList.remove("shadow");
+    stepers[2].classList.add("shadow");
+        contentQuestion.classList.add("hide");
+        resultFinal.classList.remove("hide");
+        resultFinal.innerHTML=`<div>Restez chez vous au maximum en attendant que les symptômes disparaissent. Prenez votre température deux fois par jour. Rappel des mesures d’hygiène.</div>`
+    
+})
+
+// 
--- Starter-file/gulpfile.js
+++ Starter-file/gulpfile.js
@@ -3,6 +3,11 @@ const sass = require('gulp-sass');
 const autoprefix = require('gulp-autoprefixer');
 const minifycss = require('gulp-minify-css');
 const concat = require('gulp-concat');
+const imagemin = require('gulp-imagemin');
+
+const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
+
 sass.compiler = require('node-sass');
 
 gulp.task("sass", function(){
@@ -30,7 +35,29 @@ gulp.task('coppyHtml',async function(){
     .pipe(gulp.dest('dist'));
 });
 
+gulp.task('images', function(){
+    return gulp.src('src/img/*.+(png|jpg|jpeg|gif|svg)')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/img'))
+});
 
-gulp.task('build', gulp.series('sass', 'style','coppyHtml'));
+//script paths
+
+var jsFiles = 'src/scripts/*.js',
+jsDest = 'dist/scripts';
+  
+gulp.task('scripts', function() {
+  return gulp.src(jsFiles)
+      .pipe(concat('scripts.js'))
+      .pipe(gulp.dest(jsDest))
+      .pipe(rename('scripts.min.js'))
+      .pipe(uglify())
+      .pipe(gulp.dest(jsDest));
+});
+
+  
+
+
+gulp.task('build', gulp.series('sass', 'style','coppyHtml', 'images', 'scripts'));
 
 
